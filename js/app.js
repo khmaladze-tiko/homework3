027f5db36cd776a37719a911a1d6ac4f4873b688
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-01-24 20:48:58 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("go")
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const colors = [
+    "#FF5733",
+    "#F9FF33",
+    "#4471FF",
+    "red",
+    "green",
+    "rgb(85, 255, 51)",
+];
+
+button.addEventListener("click", () => {
+    let hexColor = colors[getRandomNumber()];
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+function getRandomNumber() {
+   return Math.floor(Math.random() * colors.length);
+}
+
